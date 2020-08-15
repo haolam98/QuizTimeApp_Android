ee9d9581aba312c915d5440d880f7aa18197b3aa
Hao Lam <lamtuhao98@gmail.com>
2020-08-15 12:31:54 -0700

    Create Splash Screen Actity + Add icons to project Fix 2 warning: 1 from "Consider adding androidFullBackupContent", 2 from "app is not indexable by google search"

--- app/src/main/java/com/example/quiztimeapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/quiztimeapp/SplashScreenActivity.java
@@ -2,12 +2,33 @@ package com.example.quiztimeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainPageActivity.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#FFFFFF"))
+                .withBeforeLogoText("WELCOME TO QUIZ TIME APP")
+                .withFooterText("Designed and Developed By Hao Lam")
+                .withLogo(R.mipmap.quiz_time_icon);
+        config.getBeforeLogoTextView().setTextColor(Color.BLACK);
+        config.getBeforeLogoTextView().setTextSize(20);
+        config.getFooterTextView().setTextColor(Color.BLACK);
+        config.getFooterTextView().setTextSize(10);
+
+        View ESScreen = config.create();
+        setContentView(ESScreen);
+
     }
 }
