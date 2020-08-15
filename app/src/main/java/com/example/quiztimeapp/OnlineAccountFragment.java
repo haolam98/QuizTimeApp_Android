01fcce77f82419c5796d5b0675857e9c59656dfe
Hao Lam <lamtuhao98@gmail.com>
2020-08-15 13:26:36 -0700

    Create Fragments For Bottom Nav Tab Bar on Main Page

---
+++ app/src/main/java/com/example/quiztimeapp/OnlineAccountFragment.java
@@ -0,0 +1,31 @@
+package com.example.quiztimeapp;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OnlineAccountFragment extends Fragment {
+
+
+    public OnlineAccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_online_account, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/quiztimeapp/QuickAcessFragment.java
@@ -0,0 +1,31 @@
+package com.example.quiztimeapp;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class QuickAcessFragment extends Fragment {
+
+
+    public QuickAcessFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quick_acess, container, false);
+    }
+
+}
