3081984f2732e5d32be8fd957c2ef21c766d733d
Hao Lam <lamtuhao98@gmail.com>
2020-08-15 22:57:20 -0700

    Create and design layouts for quiz-making feature.

--- app/src/main/java/com/example/quiztimeapp/MainPageActivity.java
+++ app/src/main/java/com/example/quiztimeapp/MainPageActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -20,12 +22,14 @@ public class MainPageActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.main_page_bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
         //initialize Quick Access fragement
+        bottomNavigationView.setSelectedItemId(R.id.quick_access);
         getSupportFragmentManager().beginTransaction().replace(R.id.main_page_container,new QuickAcessFragment()).commit();
+
     }
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment frag = new QuickAcessFragment();
+            Fragment frag = null;
             switch (menuItem.getItemId())
             {
                 case R.id.quick_access:
@@ -34,7 +38,11 @@ public class MainPageActivity extends AppCompatActivity {
                 case R.id.my_account:
                     frag = new OnlineAccountFragment();
                     break;
-
+                case R.id.offline_storage:
+                    frag = new OfflineStorageFragment();
+                    break;
+                    default:
+                        frag = new QuickAcessFragment();
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.main_page_container,frag).commit();
             return true;
