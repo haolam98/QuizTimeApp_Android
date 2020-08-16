f5a582fbcb70f610b0d9a74cac813da90ee05060
Hao Lam <lamtuhao98@gmail.com>
2020-08-15 23:11:25 -0700

    Continue adding activities and layouts for quiz-making feature

---
+++ app/src/main/java/com/example/quiztimeapp/SaveAndChoiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.quiztimeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaveAndChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_and_choice);
+    }
+}
---
+++ app/src/main/java/com/example/quiztimeapp/ShareQuizActivity.java
@@ -0,0 +1,14 @@
+package com.example.quiztimeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShareQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_quiz);
+    }
+}
